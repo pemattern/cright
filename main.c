670d3d875c4fcd74bc61fe6803edc0719ec64ee6
pemattern <pemattern@proton.me>
2023-09-30 07:20:01 +0200

    added agrument for backlight device and step

--- main.c
+++ main.c
@@ -4,25 +4,44 @@
 int main(int argc, char **argv) {
   FILE *pfile;
   FILE *pmaxfile;
-  pfile = fopen("/sys/class/backlight/intel_backlight/brightness", "w+");
-  pmaxfile = fopen("/sys/class/backlight/intel_backlight/max_brightness", "r");
+  char path[256];
+  char maxpath[256];
+  strcpy(path, "/sys/class/backlight/");
 
   int value, maxvalue;
   fscanf(pmaxfile, "%d", &maxvalue);
   fscanf(pfile, "%d", &value);
 
-  if (argc > 1) {
-    if (strcmp(argv[1], "add") == 0) {
-      value += 10;
+  if (argc > 3) {
+    strcat(path, argv[1]);
+    strcpy(maxpath, path);
+    strcat(path, "/brightness");
+    strcat(maxpath, "/max_brightness");
+
+    pfile = fopen(path, "w+");
+    pmaxfile = fopen(maxpath, "r");
+
+    if (pfile == NULL || pmaxfile == NULL)
+    {
+      fclose(pfile);
+      fclose(pmaxfile);
+      return 1;
+    }  
+
+    int step;
+    sscanf(argv[3], "%d", &step);
+
+    if (strcmp(argv[2], "add") == 0) {
+      value += step;
     }
-    if (strcmp(argv[1], "reduce") == 0) {
-      value -= 10;
+    if (strcmp(argv[2], "reduce") == 0) {
+      value -= step;
     }
   }
 
   if (value < 0)
     value = 0;
-  if (value > maxvalue)
+  else if (value > maxvalue)
     value = maxvalue;
 
   fprintf(pfile, "%d", value);
